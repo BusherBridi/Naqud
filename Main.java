02bcd7f31615edfcaf2e2b1023b125675573ca90
Busher <bridibusher@gmail.com>
2021-10-19 20:25:02 -0700

    testing fixes

--- Main.java
+++ Main.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Menu mainMenu = new Menu("Main Menu", 3);
         mainMenu.set_menuButtonTitle(0, "Set up");
         mainMenu.set_menuButtonTitle(1, "Input");
         mainMenu.set_menuButtonTitle(2, "View");
-        int userSelection = mainMenu.printAndReturnInput();
+        mainMenu.print();
         // Menu Logic:
+        int userSelection = sc.nextInt();
         switch (userSelection) {
-            case 1:
-                System.out.println("Set up");
-                String name;
-                String description;
-                float netPay;
-                Scanner setupScanner = new Scanner(System.in);
-                name = setupScanner.nextLine();
-                break;
-            case 2:
-                System.out.println("Input");
-                break;
-            case 3:
-                System.out.println("View");
-                break;
-            case 0:
-                System.out.println("Exit");
-                break;
-            default:
-                System.out.println("Invalid Choice");
+        case 1:
+            System.out.println("Set up");
+            Portfolio myPortfolio = Portfolio.userSetup();
+            myPortfolio.print();
+            break;
+        case 2:
+            System.out.println("Input");
+            break;
+        case 3:
+            System.out.println("View");
+            break;
+        case 0:
+            System.out.println("Exit");
+            break;
+        default:
+            System.out.println("Invalid Choice");
         }
-
+        sc.close();
     }
+
 }
