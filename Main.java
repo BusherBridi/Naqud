1d7ad2f7742cd5766c7db1708794082405fd5930
Busher <bridibusher@gmail.com>
2021-10-23 22:46:32 -0700

    Removed some features for testing

--- Main.java
+++ Main.java
@@ -7,27 +7,33 @@ class Main {
         mainMenu.set_menuButtonTitle(0, "Set up");
         mainMenu.set_menuButtonTitle(1, "Input");
         mainMenu.set_menuButtonTitle(2, "View");
-        mainMenu.print();
+
+        Portfolio myPortfolio = new Portfolio("Test", "test portfolio", 6000);
+        myPortfolio.addTransaction("rent", 1800, "Rent money");
+        myPortfolio.addTransaction("Groceries", 200, "gotta eat");
+        myPortfolio.addTransaction("Investments", 500, "workday.inc");
+        System.out.printf("total expense: %f\n", myPortfolio.calculateFutureBalance(4));
+        // mainMenu.print();
         // Menu Logic:
-        int userSelection = sc.nextInt();
-        switch (userSelection) {
-        case 1:
-            System.out.println("Set up");
-            Portfolio myPortfolio = Portfolio.userSetup(sc);
-            myPortfolio.print();
-            break;
-        case 2:
-            System.out.println("Input");
-            break;
-        case 3:
-            System.out.println("View");
-            break;
-        case 0:
-            System.out.println("Exit");
-            break;
-        default:
-            System.out.println("Invalid Choice");
-        }
+        // int userSelection = sc.nextInt();
+        // switch (userSelection) {
+        // case 1:
+        // System.out.println("Set up");
+        // Portfolio myPortfolio = Portfolio.userSetup(sc);
+        // myPortfolio.print();
+        // break;
+        // case 2:
+        // System.out.println("Input");
+        // break;
+        // case 3:
+        // System.out.println("View");
+        // break;
+        // case 0:
+        // System.out.println("Exit");
+        // break;
+        // default:
+        // System.out.println("Invalid Choice");
+        // }
         sc.close();
     }
 
