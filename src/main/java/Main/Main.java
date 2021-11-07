606700aae7fea85911b4d0d0d6726c684cbe613b
User <bridibusher@gmail.com>
2021-11-06 21:56:00 -0700

    Enhanced menu logic

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -16,48 +16,33 @@ class Main {
         mainMenu.set_menuButtonTitle(1, "Input");
         mainMenu.set_menuButtonTitle(2, "View");
 
-        Portfolio myPortfolio = new Portfolio("Test", "memo", 6000);
-        myPortfolio.addTransaction("rent", -1800, "Rent money");
-        myPortfolio.addTransaction("Groceries", -200, "gotta eat");
-        myPortfolio.addTransaction("Investments", -500, "workday.inc");
-        myPortfolio.addRecurringTransaction("Debt", -100, "memo", 1);
-        System.out.printf("total expense: %f\n", myPortfolio.calculateFutureBalance(4));
-        writeToFile("output", myPortfolio);
-        String[] headers = { "", "Name", "Diameter", "Mass", "Atmosphere" };
-        String[][] data = { { "1", "Mercury", "0.382", "0.06", "minimal" },
-                { "2", "Venus", "0.949", "0.82", "Carbon dioxide, Nitrogen" },
-                { "3", "Earth", "1.000", "1.00", "Nitrogen, Oxygen, Argon" },
-                { "4", "Mars", "0.532", "0.11", "Carbon dioxide, Nitrogen, Argon" } };
-
-        System.out.println(AsciiTable.getTable(headers, data));
-        // PrettyTable table = new PrettyTable("Firstname", "Lastname", "Email",
-        // "Phone");
-        // table.addRow("John", "Doe", "johndoe@nothing.com", "+2137999999");
-        // table.addRow("Jane", "Doe", "janedoe@nothin.com", "+2137999999");
-        // System.out.println(table);
-
-        // myPortfolio.print();
-        // mainMenu.print();
+        
         // Menu Logic:
-        // int userSelection = sc.nextInt();
-        // switch (userSelection) {
-        // case 1:
-        // System.out.println("Set up");
-        // Portfolio myPortfolio = Portfolio.userSetup(sc);
-        // myPortfolio.print();
-        // break;
-        // case 2:
-        // System.out.println("Input");
-        // break;
-        // case 3:
-        // System.out.println("View");
-        // break;
-        // case 0:
-        // System.out.println("Exit");
-        // break;
-        // default:
-        // System.out.println("Invalid Choice");
-        // }
+        mainMenu.print();
+        int userSelection = sc.nextInt();
+        do {
+            
+            switch (userSelection) {
+            case 1:
+                System.out.println("Set up");
+                Portfolio myPortfolio = Portfolio.userSetup(sc);
+                myPortfolio.print();
+                break;
+            case 2:
+                System.out.println("Input");
+                break;
+            case 3:
+                System.out.println("View");
+                break;
+            case 0:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Invalid Choice");
+            }
+            mainMenu.print();
+            userSelection = sc.nextInt();
+        } while (userSelection != 0);
         sc.close();
     }
 
